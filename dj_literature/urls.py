8af2eb4cd1a498bc462e2fba2b87c49a8a5e1320
NameNolen <renpeng18@live.com>
2018-11-22 21:25:43 +0800

    rest-api viewApi

--- dj_literature/urls.py
+++ dj_literature/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-# todo 还有好多没有做
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('collections/', views.CollectionList.as_view(), name='collection_list'),
