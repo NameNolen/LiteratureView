c5673ee1cfd626add39244a3a407a7b92815dc96
NameNolen <renpeng18@live.com>
2019-03-02 18:30:03 +0800

    测试

---
+++ dj_literature/templates/dj_literature/precache-manifest.289540fdd304aa8481fa45e142e47d94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7026af2d206f5d55240f9f0be16d79b",
+    "url": "./static/media/menu.d7026af2.svg"
+  },
+  {
+    "revision": "c40eed72b42fd13dda9560694bb1e1f6",
+    "url": "./static/media/arrow.c40eed72.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "42ef4af614a8f448d407",
+    "url": "./static/js/main.42ef4af6.chunk.js"
+  },
+  {
+    "revision": "39d27e4f6efea377666e",
+    "url": "./static/js/1.39d27e4f.chunk.js"
+  },
+  {
+    "revision": "42ef4af614a8f448d407",
+    "url": "./static/css/main.d4355251.chunk.css"
+  },
+  {
+    "revision": "d49665aa3ddeff8fd5c2d3734c759700",
+    "url": "./index.html"
+  }
+];
